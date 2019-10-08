9078e3a64a7206bf0ed42c762ec362fb4a04755f
Daniel Schrøder <schroeder1004@hotmail.com>
2019-10-08 20:28:50 +0200

    Added Filtering, finished entities and some validation

---
+++ BookShop.Core.Entities/Filter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.Core.Entities
+{
+    public class Filter
+    {
+        public int CurrentPage { get; set; }
+        public int ItemsPrPage { get; set; }
+        public String OrderBy { get; set; }
+        public String SortBy { get; set; }
+    }
+}
---
+++ BookShop.Core.Entities/IsNullOrEmpty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace BookShop.Core.Entities
+{
+    public static class IsNullOrEmpty
+    {
+        public static String Check(object myObject)
+        {
+            foreach (PropertyInfo pi in myObject.GetType().GetProperties())
+            {
+                if (pi.PropertyType == typeof(string))
+                {
+                    string value = (string) pi.GetValue(myObject);
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        throw new InvalidDataException(pi.Name + " is missing");
+                    }
+                }
+            }
+            return null;
+
+        }
+    }
+}
